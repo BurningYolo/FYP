96ac986a6d5fa7abe60a3bf501fc2faa5d642607
BurningYolo <hishamzahid123@gmail.com>
2022-07-27 10:50:23 +0500

    .

--- js_backend_stuff/actual_js.js
+++ js_backend_stuff/actual_js.js
@@ -15,6 +15,31 @@ $(document).ready(function () {
 });
 
 
+function show_credit_card()
+{
+    document.getElementById("credit_card_form").hidden=false; 
+    document.getElementById("paypal_form").hidden=true; 
+    document.getElementById("bank_form").hidden=true; 
+    
+}
+
+function show_paypal()
+{
+    document.getElementById("credit_card_form").hidden=true; 
+    document.getElementById("paypal_form").hidden=false; 
+    document.getElementById("bank_form").hidden=true; 
+    
+}
+
+function show_bank()
+{
+    document.getElementById("credit_card_form").hidden=true; 
+    document.getElementById("paypal_form").hidden=true; 
+    document.getElementById("bank_form").hidden=false; 
+    
+}
+
+
 
 
 
